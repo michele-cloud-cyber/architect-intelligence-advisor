b321dcfac2f0e469ef9d13aeecc623c72c4ef5c7
michele-cloud-cyber <michele.silves@icloud.com>
2026-07-12 11:06:03 +0200

    Integrate Security Hub discovery

--- sr/collectors/collector.py
+++ sr/collectors/collector.py
@@ -62,16 +62,52 @@ ec2 = boto3.client("ec2", region_name="eu-west-1")
         "eu-central-1"
     ]
 
-    landing_zone.add_finding(
-        "CloudTrail enabled."
-    )
+   cloudtrail = boto3.client("cloudtrail", region_name="eu-west-1")
 
-    landing_zone.add_finding(
-        "GuardDuty enabled."
-    )
+try:
+    trails = cloudtrail.describe_trails()
 
-    landing_zone.add_finding(
-        "Security Hub enabled."
-    )
+    if trails["trailList"]:
+        landing_zone.add_finding("CloudTrail enabled.")
 
-    print("Collection completed.")
+        print("CloudTrail Trails:")
+        for trail in trails["trailList"]:
+            print(f"- {trail['Name']}")
+    else:
+        landing_zone.add_finding("CloudTrail not enabled.")
+
+except ClientError as error:
+    print(f"Unable to retrieve CloudTrail information: {error}")
+    landing_zone.add_finding("CloudTrail status unknown.")
+    
+
+  guardduty = boto3.client("guardduty", region_name="eu-west-1")
+
+try:
+    response = guardduty.list_detectors()
+
+    if response["DetectorIds"]:
+        landing_zone.add_finding("GuardDuty enabled.")
+
+        print("GuardDuty Detectors:")
+        for detector in response["DetectorIds"]:
+            print(f"- {detector}")
+    else:
+        landing_zone.add_finding("GuardDuty not enabled.")
+
+except ClientError as error:
+    print(f"Unable to retrieve GuardDuty information: {error}")
+    landing_zone.add_finding("GuardDuty status unknown.")
+    
+
+  securityhub = boto3.client("securityhub", region_name="eu-west-1")
+
+try:
+    securityhub.describe_hub()
+
+    landing_zone.add_finding("Security Hub enabled.")
+    print("Security Hub is enabled.")
+
+except ClientError as error:
+    landing_zone.add_finding("Security Hub not enabled.")
+    print(f"Security Hub not available: {error}")
